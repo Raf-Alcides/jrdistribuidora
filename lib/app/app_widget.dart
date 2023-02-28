a274b9a60c54b6ad57a9bd883e0af845ac187fa7
Rafael Alcides <devrafalcides@gmail.com>
2023-02-28 19:20:30 -0300

    config modular e asuca

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:asuka/asuka.dart' as asuka;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Modular.setObservers([asuka.Asuka.asukaHeroController]);
+
+    return ScreenUtilInit(
+        designSize: const Size(390, 844),
+        builder: (_, __) {
+          return MaterialApp.router(
+            title: 'Jr Distribuidora',
+            builder: asuka.Asuka.builder,
+            theme: ThemeData(primarySwatch: Colors.blue),
+            routeInformationParser: Modular.routeInformationParser,
+            routerDelegate: Modular.routerDelegate,
+          );
+        });
+  }
+}
