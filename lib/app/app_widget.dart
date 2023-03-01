8d22165193f78eeee7b659d8ac4d8b5f0e1ae9e8
Rafael Alcides <devrafalcides@gmail.com>
2023-02-28 23:11:09 -0300

    update app_widget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -9,6 +9,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Modular.setObservers([asuka.Asuka.asukaHeroController]);
+    Modular.setInitialRoute('/auth');
 
     return ScreenUtilInit(
         designSize: const Size(390, 844),
