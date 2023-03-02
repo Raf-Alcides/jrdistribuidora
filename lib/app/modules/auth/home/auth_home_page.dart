f95ce49e9bb2af41fff8bd5189ad294c433a7cf8
Rafael Alcides <devrafalcides@gmail.com>
2023-03-02 00:38:05 -0300

    splasScreen

--- lib/app/modules/auth/home/auth_home_page.dart
+++ lib/app/modules/auth/home/auth_home_page.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AuthHomePage extends StatelessWidget {
+  const AuthHomePage({Key? key}) : super(key: key);
 
-  const AuthHomePage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: const Center(child: Text('oi'),),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Image.asset(
+        'assets/imagens/',
+        width: 130,
+        height: 162,
+        fit: BoxFit.contain,
+      ),
+    ));
   }
 }
